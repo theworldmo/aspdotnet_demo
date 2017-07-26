3124c254c094e69704ef0c1fe4eb7352f46d0de3
Jeff Mo <mo0118@gmail.com>
2017-07-26 09:05:24 -0700

    add About page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -17,5 +17,10 @@ namespace WebApplicationBasic.Controllers
         {
             return View();
         }
+
+        public IActionResult About()
+        {
+            return View();
+        }
     }
 }
